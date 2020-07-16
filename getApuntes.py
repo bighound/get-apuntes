bd7060959a9c842e1d2a260aa6f8c5e1dbaaf059
Bighound <bighound95@hotmail.com>
2020-07-16 12:25:30 +0200

    Actualización de algunos aspectos
    - Cambio de nombre de algunas variables.
    - Control de errores

--- getApuntes.py
+++ getApuntes.py
@@ -13,8 +13,8 @@ import subprocess
 import sys
 from bs4 import BeautifulSoup
 
-def concurrentDownload(i, asignaturas, user, pwd):
-    subprocess.run(["wget", "-r", "https://aulavirtual.um.es/dav/{0}".format(asignaturas[i]), "--user={0}".format(user),"--password={0}".format(pwd),"2>&2"])
+def concurrentDownload(i, subjects, user, pwd):
+    subprocess.run(["wget", "-r", "https://aulavirtual.um.es/dav/{0}".format(subjects[i]), "--user={0}".format(user),"--password={0}".format(pwd),"2>&2"])
 
 def login(user, pwd):
     s       = requests.Session()
@@ -23,26 +23,33 @@ def login(user, pwd):
     payload = {'username':user, 'password':pwd, '_eventId':'submit'}
     
     req   = s.post(url, headers=headers, data=payload)
-    if req.status_code !=200:
+    if req.status_code != 200:
         print("[!] El login es incorrecto, prueba con otras credenciales.")
         sys.exit(1)
 
     soup  = BeautifulSoup(req.text,'html.parser')
     value = soup.find('input', {'name': 'execution'}).get('value')
+    if (value is None):
+        print("[!] No se ha podido obtener la información necesaria.")
+        sys.exit(1)
 
     payload = {'username':user, 'password':pwd, 'execution':value, '_eventId':'submit','geolocation':''}
     req  = s.post(url, data=payload)
+    if req.status_code != 200:
+        print("[!] El login es incorrecto, prueba con otras credenciales.")
+        sys.exit(1)
+
     page = req.text
     soup = BeautifulSoup(page,'html.parser')
 
-    asignaturas=[]
-    for asign in soup.find_all('a', {'href': re.compile(".*_N_N")}):
-        enlace = asign.get('href').split('/')
-        asignaturas.append(enlace[len(enlace) - 1])
+    subjects=[]
+    for subj in soup.find_all('a', {'href': re.compile(".*_N_N")}):
+        link = subj.get('href').split('/')
+        subjects.append(link[len(link) - 1])
     
     threads = list()
-    for i in range(len(asignaturas)):
-        t = threading.Thread(target=concurrentDownload, args=(i, asignaturas, user, pwd))
+    for i in range(len(subjects)):
+        t = threading.Thread(target=concurrentDownload, args=(i, subjects, user, pwd))
         threads.append(t)
         t.start()
 
@@ -60,15 +67,15 @@ def main():
         print("[!] Debes de introducir tanto el usuario como tu contraseña")
         sys.exit(1)
 
-    if (args.user == ""):
-        print("[!] El usuario no puede estar vacío.")
+    if (args.user == "" or args.pwd == ""):
+        print("[!] El usuario o la contraseña no pueden estar vacíos.")
         sys.exit(1)
 
     user = args.user
     pwd  = args.pwd
 
-    x = re.search("[a-zA-Z]+[0-9]*@um.es", user)
-    if (x is None):
+    emailFormat = re.search("[a-zA-Z]+[0-9]*@um.es", user)
+    if (emailFormat is None):
         print("[!] El correo es incorrecto. Ej: example@um.es")
         sys.exit(1)
 
